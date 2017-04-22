0a2030d1f6229b61564fe8714efac264a770f52e
Infologs <webboostings@gmail.com>
2017-04-22 23:36:39 +0530

    Show and Hide Directives in AngularJS

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("app01", []);
+
+app.controller("controller1", function () {
+    this.ControllerName = "Login Page Controller";
+});
+
+app.controller("controller2", function () {
+    this.ControllerName = "Registration Page Controller";
+});;
+
+app.controller("controller3", function () {
+    this.Show = true;
+    this.Hide = false;
+});
